2601f4ef65f340b846f760085f3577e0cb840edd
GMrlsN <gabrielmn24@outlook.com>
2022-09-09 14:45:07 +0000

    Ajuste prueba

--- prueba.cpp
+++ prueba.cpp
@@ -1,28 +1,3 @@
-/*#include <iostream>
-#include <stdio.h>
-#include <conio.h>
-float resultado;
-float area, radio, pi;
-int a, b, c,i;
-char opcion;
-float altura, x;
-//Este programa calcula el volumen de un cilindro.
-void main()
-{   
-    printf("\n\n\nhola mundo\t\t\tacabo de insertar un tabulador\ny ahora una nueva linea\n");
-    printf("Resultado = ");
-    x = 10;
-    a = (3 + 5)*8 - (x - 4)/2;
-    for(i = 0; i < 10; i++){
-        printf( (3 + 5)*8 - (x - 4)/2);
-    }
-    printf("Radio = ");
-    scanf("%d",&radio);
-    pi = 3.141592653589793;
-    area =  pi * (radio * radio);
-    printf("Area = ");
-    printf(area);
-}*/
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
