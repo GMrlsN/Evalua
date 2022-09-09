8881bf24f78cf8cdc465d4b8bcfe64525344a37f
GMrlsN <gabrielmn24@outlook.com>
2022-09-09 14:32:16 +0000

    Ajustes para la prueba

--- prueba.cpp
+++ prueba.cpp
@@ -1,25 +1,45 @@
-#include <iostream>
+/*#include <iostream>
 #include <stdio.h>
 #include <conio.h>
 float resultado;
 float area, radio, pi;
-//int a, b, c,a,i;
-//char opcion;
-///float radio, altura, x;
-// Este programa calcula el volumen de un cilindro.
+int a, b, c,i;
+char opcion;
+float altura, x;
+//Este programa calcula el volumen de un cilindro.
 void main()
 {   
-    /*printf("hola mundo\tacabo de insertar un tabulador\ny ahora una nueva linea\n");
+    printf("\n\n\nhola mundo\t\t\tacabo de insertar un tabulador\ny ahora una nueva linea\n");
     printf("Resultado = ");
     x = 10;
-    //a = (3 + 5)*8 - (x - 4)/2;
+    a = (3 + 5)*8 - (x - 4)/2;
     for(i = 0; i < 10; i++){
-        //printf( (3 + 5)*8 - (x - 4)/2);
-    }*/
+        printf( (3 + 5)*8 - (x - 4)/2);
+    }
     printf("Radio = ");
     scanf("%d",&radio);
     pi = 3.141592653589793;
     area =  pi * (radio * radio);
     printf("Area = ");
     printf(area);
+}*/
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+float area, radio, pi, resultado;
+int a, d;
+// Este programa calcula el volumen de un cilindro.
+void main(){
+    printf("\n\n\n\t\tHolaaaaaaaaa");
+    printf("\n\n\tRadio =  ");
+    scanf("%d", &radio);
+    pi = 3.141592653589793;
+    d = 4;
+    area = pi * (radio * radio);
+    printf("\nArea = ");
+    printf(area);
+    printf("\nHola\n");
+
+    for(d = 10; d<20; d++) {
+    }
 }
